34688e01344fc9de83e0ee5d431a47aa5de90a7f
Jules <druage@gmx.com>
2017-10-17 18:31:17 -0400

    Reverted link seam to an inheritance based mocking approach.

--- unit_tests/cpp/src/test_main.cpp
+++ unit_tests/cpp/src/test_main.cpp
@@ -1,9 +1,9 @@
-//#include "test_emulator.h"
-//#include "test_libretrolibrary.h"
-//#include "test_messageserver.h"
-//#include "test_sharedmemory.h"
-//#include "test_gamepad.h"
-//#include "test_gamepadmanager.h"
+#include "test_emulator.h"
+#include "test_libretrolibrary.h"
+#include "test_messageserver.h"
+#include "test_sharedmemory.h"
+#include "test_gamepad.h"
+#include "test_gamepadmanager.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,32 +18,29 @@ int main(int argc, char *argv[])
 {
     QCoreApplication app( argc, argv );
 
-    Emulator emu;
-//    app.setAttribute(Qt::AA_Use96Dpi, true);
+    app.setAttribute(Qt::AA_Use96Dpi, true);
 
-//    QTEST_SET_MAIN_SOURCE_PATH
+    QTEST_SET_MAIN_SOURCE_PATH
 
-//    QList<QObject *> tests = {
-//        new Test_Emulator,
-//        new Test_LibretroLibrary,
-//        new Test_MessageServer,
-//        new Test_SharedMemory,
-//        new Test_Gamepad,
-//        new Test_GamepadManager,
-//    };
+    QList<QObject *> tests = {
+        new Test_Emulator,
+        new Test_LibretroLibrary,
+        new Test_MessageServer,
+        new Test_SharedMemory,
+        new Test_Gamepad,
+        new Test_GamepadManager,
+    };
 
-//    int status = 0;
-//    for ( QObject *test : tests ) {
-//        status |= QTest::qExec(test, argc, argv);
-//        delete test;
-//    }
+    int status = 0;
+    for ( QObject *test : tests ) {
+        status |= QTest::qExec(test, argc, argv);
+        delete test;
+    }
 
-//    if ( status == 0 ) {
-//        qDebug() << "\nAll tests have successfully passed!";
-//    }
+    if ( status == 0 ) {
+        qDebug() << "\nAll tests have successfully passed!";
+    }
 
-//    return status;
+    return status;
 
-    qDebug() << "Ran Test cases";
-    return 0;
 }
