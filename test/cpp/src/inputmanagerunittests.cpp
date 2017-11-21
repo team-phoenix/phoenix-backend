8613444d0bb5242e3d6881b3848f42e3d0b157f7
druage <druage@gmx.com>
2017-11-20 22:49:03 -0500

    Added 'getControllerStates' to InputManager

--- test/cpp/src/inputmanagerunittests.cpp
+++ test/cpp/src/inputmanagerunittests.cpp
@@ -33,7 +33,43 @@ SCENARIO("The input manager is able to get and set the data "
       THEN("it returns the size of the buffer") {
         REQUIRE(actualSize == 1);
       }
+    }
 
+  }
+}
+
+SCENARIO("The input manager can store controller states")
+{
+  GIVEN("") {
+
+    static QVector<qint16> expectedGamepadStates;
+
+    struct MockKeyboardBuffer {
+      void resize(int) {}
+      void fill(qint16) {}
+    };
+
+
+    struct MockGamepadUpdater {
+      void updateStates(QVector<QVector<qint16>> &buffer)
+      {
+        buffer = QVector<QVector<qint16>> { {0x01}, {0x01} };
+      }
+    };
+
+    InputManager_T<MockKeyboardBuffer, MockGamepadUpdater> subject;
+
+    WHEN("pollControllerStates() is called") {
+
+      auto beforeStates = subject.getControllerStates();
+      subject.pollControllerStates();
+      auto afterStates = subject.getControllerStates();
+
+      THEN("the controller states will have been updated and filled in") {
+        REQUIRE(beforeStates != afterStates);
+        QVector<QVector<qint16>> expectedBuffer({ {0x01}, {0x01} });
+        REQUIRE(afterStates == expectedBuffer);
+      }
     }
   }
 }
