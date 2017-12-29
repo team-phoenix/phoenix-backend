eddd5e9725d1a82a8c51059383f71d232b800422
druage <druage@gmx.com>
2017-12-29 17:08:10 -0500

    Removed test code from main

--- frontend/cpp/src/main.cpp
+++ frontend/cpp/src/main.cpp
@@ -12,24 +12,6 @@ int main(int argc, char* argv[])
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
 
-  LibraryDb libDb;
-  libDb.insert(GameEntry(QVariantHash({
-    { "absoluteFilePath", "4575474" },
-    { "sha1Checksum", "fhhjh" },
-  })));
-  auto entries = libDb.findAllByGameEntry();
-
-  for (GameEntry &row : entries) {
-    qDebug() << row.rowIndex << row.absoluteFilePath << row.sha1Checksum;
-  }
-
-  OpenVgDb db;
-  auto rows = db.findRomsBySha1("56FE858D1035DCE4B68520F457A0858BAE7BB16D");
-
-  for (Rom &row : rows) {
-    qDebug() << row.romID << row.romSize << row.systemID;
-  }
-
   qmlRegisterType<GameEntryDbModel>("vg.phoenix.models", 1, 0, "GameEntryDbModel");
 
   QQmlApplicationEngine engine;
